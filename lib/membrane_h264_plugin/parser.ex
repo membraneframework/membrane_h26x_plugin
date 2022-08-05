0ee3d5e67bde37dba1d4f7cb9c23822bf2fd1322
Dominik Artur Wołek <dominik.wolek@swmansion.com>
2022-08-05 15:21:51 +0200

    Use BinaryParser

--- lib/membrane_h264_plugin/parser.ex
+++ lib/membrane_h264_plugin/parser.ex
@@ -7,6 +7,7 @@ defmodule Membrane.H264.Parser do
 
   alias __MODULE__
   alias Membrane.{Buffer, H264}
+  alias Membrane.H264.BinaryParser
 
   @ending_nalu_types [:end_of_seq, :end_of_stream]
   @default_last %{unprefixed_poslen: {0, 0}, prefixed_poslen: {0, 0}, type: :reserved}
@@ -70,8 +71,10 @@ defmodule Membrane.H264.Parser do
     state = %{
       caps: opts.caps,
       metadata: %{},
-      partial_au: {[], <<>>},
-      partial_nalu: <<>>
+      partial_nalu: <<>>,
+      parser_buffer: [],
+      parser_state: :first,
+      vcl_state: nil
     }
 
     {:ok, state}
@@ -86,18 +89,26 @@ defmodule Membrane.H264.Parser do
   def handle_process(:input, %Membrane.Buffer{} = buffer, _ctx, state) do
     payload = state.partial_nalu <> buffer.payload
 
-    {access_units, payload, state} =
+    {new_payload, state} =
       payload
       |> Parser.NALu.parse()
-      |> Enum.chunk_while([], &chunk_au_fun/2, &{:cont, Enum.reverse(&1), []})
       |> save_partial_nalu(payload, state)
-      |> then(fn {aus, payload, state} -> add_partial_au(aus, payload, state) end)
-      |> then(fn {aus, payload, state} -> save_partial_au(aus, payload, state) end)
+
+    {_nalus, parser_buffer, parser_state, vcl_state, access_units} =
+      BinaryParser.split_binary_into_access_units(
+        new_payload,
+        state.parser_buffer,
+        state.parser_state,
+        state.vcl_state
+      )
+
+    state = %{state | parser_buffer: buffer, parser_state: parser_state, vcl_state: vcl_state}
 
     if access_units == [] do
       {:ok, state}
     else
-      buffers = Enum.map(access_units, &wrap_into_buffer(&1, payload, state.metadata))
+      # FIXME: don't pass hardcoded empty metadata
+      buffers = Enum.map(access_units, &wrap_into_buffer(&1, new_payload, state.metadata))
 
       {{:ok, buffer: {:output, buffers}}, state}
     end
@@ -122,61 +133,31 @@ defmodule Membrane.H264.Parser do
     {{:ok, buffer: {:output, buffers}, end_of_stream: :output}, new_state}
   end
 
-  defp chunk_au_fun(nalu, []), do: {:cont, [nalu]}
-
-  defp chunk_au_fun(nalu, acc) do
-    case nalu.type do
-      :aud -> {:cont, Enum.reverse(acc), [nalu]}
-      :end_of_seq -> {:cont, Enum.reverse([nalu | acc]), []}
-      :end_of_stream -> {:halt, Enum.reverse([nalu | acc])}
-      _type -> {:cont, [nalu | acc]}
-    end
+  defp save_partial_nalu([], payload, state) do
+    new_state = Map.update(state, :partial_nalu, <<>>, &(&1 <> payload))
+    {<<>>, new_state}
   end
 
-  defp save_partial_nalu(access_units, payload, state) do
+  defp save_partial_nalu(nalus, payload, state) do
+    last_nalu_start =
+      nalus
+      |> List.last()
+      |> Map.get(:prefixed_poslen)
+      |> then(fn {start, _len} -> start end)
+
     payload_size = byte_size(payload)
-
-    partial_start =
-      access_units
-      |> List.last([@default_last])
-      |> parsed_length()
-      |> then(fn {start, len} -> start + len end)
-
-    partial_nalu = :binary.part(payload, partial_start, payload_size - partial_start)
-    new_payload = :binary.part(payload, 0, partial_start)
+    partial_nalu = :binary.part(payload, last_nalu_start, payload_size - last_nalu_start)
+    new_payload = :binary.part(payload, 0, last_nalu_start)
     new_state = %{state | partial_nalu: partial_nalu}
-    {access_units, new_payload, new_state}
+
+    {new_payload, new_state}
   end
 
-  defp add_partial_au(access_units, payload, state) do
-    {au_parsed, au_payload} = state.partial_au
-
-    size = byte_size(au_payload)
-    update_size = fn {from, len} -> {from + size, len} end
-
-    [head | tail] = Enum.map(access_units, &update_parsed_poslens(&1, update_size))
-
-    {[au_parsed ++ head | tail], au_payload <> payload, state}
-  end
-
-  defp save_partial_au(access_units, payload, state) do
-    partial_parsed = List.last(access_units, [@default_last])
-    last_partial_nalu = List.last(partial_parsed, @default_last)
-
-    if last_partial_nalu.type in @ending_nalu_types do
-      {access_units, payload, %{state | partial_au: {[], <<>>}}}
-    else
-      {start, len} = parsed_length(partial_parsed)
-      partial_payload = :binary.part(payload, start, len)
-
-      update_start = fn {from, len} -> {from - start, len} end
-      new_partial_parsed = update_parsed_poslens(partial_parsed, update_start)
-
-      new_state = %{state | partial_au: {new_partial_parsed, partial_payload}}
-      new_access_units = Enum.drop(access_units, -1)
-      new_payload = :binary.part(payload, 0, start)
-      {new_access_units, new_payload, new_state}
-    end
+  defp wrap_into_buffer(access_unit, payload, metadata) do
+    access_unit
+    |> then(&parsed_length/1)
+    |> then(fn {start, len} -> :binary.part(payload, start, len) end)
+    |> then(&%Buffer{payload: &1, metadata: metadata})
   end
 
   defp parsed_length([]), do: {0, 0}
@@ -194,18 +175,5 @@ defmodule Membrane.H264.Parser do
       |> then(fn {last_start, last_len} -> last_start + last_len - start end)
 
     {start, len}
-  end
-
-  defp update_parsed_poslens(parsed, fun) do
-    parsed
-    |> update_in([Access.all(), :prefixed_poslen], fun)
-    |> update_in([Access.all(), :unprefixed_poslen], fun)
-  end
-
-  defp wrap_into_buffer(access_unit, payload, metadata) do
-    access_unit
-    |> then(&parsed_length/1)
-    |> then(fn {start, len} -> :binary.part(payload, start, len) end)
-    |> then(&%Buffer{payload: &1, metadata: metadata})
   end
 end
