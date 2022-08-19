68a225de150cc3608fc9b0bb09b3ebc9d03f262c
varsill <lukasz.kita0@gmail.com>
2022-08-19 12:16:08 +0200

    Format the test

--- test/parser/access_unit_splitter_test.exs
+++ test/parser/access_unit_splitter_test.exs
@@ -9,7 +9,8 @@ defmodule AccessUnitSplitterTest do
     for file_name <- dir_files do
       binary = File.read!(file_name)
       aus = Membrane.H264.AccessUnitSplitter.split_binary_into_access_units(binary)
-      au_lengths = for au <- aus, do: Enum.reduce(au, 0, fn nalu, acc -> (nalu.prefixed_poslen |> elem(1))+acc  end)
+      au_lengths = for au <- aus, do: Enum.reduce(au, 0, fn nalu, acc ->
+        (nalu.prefixed_poslen |> elem(1))+acc  end)
 
       {:ok, decoder_ref} = Parser.create()
       {:ok, au_lengths_ffmpeg, _decoding_order_numbers, _presentation_order_numbers, _changes} =
