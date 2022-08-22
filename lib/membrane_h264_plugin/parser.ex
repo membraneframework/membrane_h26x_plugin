135cd8f7416c83620c7d22c7261c767d55e20a37
varsill <lukasz.kita0@gmail.com>
2022-08-22 12:10:08 +0200

    Add function to read the profile of the h264 stream. Add tests checking that functionality

--- lib/membrane_h264_plugin/parser.ex
+++ lib/membrane_h264_plugin/parser.ex
@@ -80,7 +80,8 @@ defmodule Membrane.H264.Parser do
 
   @impl true
   def handle_caps(:input, _caps, _ctx, state) do
-    {{:ok, caps: {:output, state.caps}}, state}
+    # {{:ok, caps: {:output, state.caps}}, state}
+    {:ok, state}
   end
 
   @impl true
@@ -118,10 +119,32 @@ defmodule Membrane.H264.Parser do
     if access_units == [] do
       {{:ok, actions}, state}
     else
-      # FIXME: don't pass hardcoded empty metadata
+      nalus = Enum.flat_map(access_units, & &1)
 
+      # FIXME: don't pass hardcoded empty metadata
+      sps_nalu = Enum.find(nalus, nil, fn nalu -> nalu.type == :sps end)
+      profile = if sps_nalu != nil, do: get_profile(sps_nalu)
       buffers = Enum.map(access_units, &wrap_into_buffer(&1, payload, state.metadata))
       new_actions = [{:buffer, {:output, buffers}} | actions]
+
+      new_actions =
+        if profile != nil,
+          do:
+            [
+              caps:
+                {:output,
+                 %H264{
+                   alignment: :au,
+                   framerate: {0, 1},
+                   height: 720,
+                   nalu_in_metadata?: false,
+                   profile: profile,
+                   stream_format: :byte_stream,
+                   width: 1280
+                 }}
+            ] ++ new_actions,
+          else: new_actions
+
       {{:ok, new_actions}, state}
     end
   end
@@ -150,5 +173,38 @@ defmodule Membrane.H264.Parser do
       |> then(fn {last_start, last_len} -> last_start + last_len - start end)
 
     {start, len}
+  end
+
+  @profiles_description [
+    high_cavlc_4_4_4_intra: [profile_idc: 44],
+    constrained_baseline: [profile_idc: 66, constraint_set1: 1],
+    baseline: [profile_idc: 66],
+    main: [profile_idc: 77],
+    extended: [profile_idc: 88],
+    constrained_high: [profile_idc: 100, constraint_set4: 1, constraint_set5: 1],
+    progressive_high: [profile_idc: 100, constraint_set4: 1],
+    high: [profile_idc: 100],
+    high_10_intra: [profile_idc: 110, constraint_set3: 1],
+    high_10: [profile_idc: 110],
+    hight_4_2_2_intra: [profile_idc: 122, constraint_set3: 1],
+    high_4_2_2: [profile_idc: 122],
+    high_4_4_4_intra: [profile_idc: 244, constraint_set3: 1],
+    high_4_4_4_predictive: [profile_idc: 244]
+  ]
+
+  defp get_profile(sps_nalu) do
+    fields = sps_nalu.parsed_fields
+
+    {profile_name, _constraints_list} =
+      @profiles_description
+      |> Enum.find({nil, nil}, fn {_profile_name, constraints_list} ->
+        constraints_list
+        |> Enum.all?(fn {key, value} ->
+          Map.has_key?(fields, key) and fields[key] == value
+        end)
+      end)
+
+    if profile_name == nil, do: raise("Cannot read the profile name based on SPS's fields.")
+    profile_name
   end
 end
