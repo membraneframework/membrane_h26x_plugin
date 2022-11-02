6494b850e13fac75aa6f66781fba25de601dc9bc
varsill <lukasz.kita0@gmail.com>
2022-11-02 12:18:24 +0100

    Bump to v0.1.1

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.H264.TODO.Mixfile do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.1.1"
   @github_url "https://github.com/membraneframework-labs/membrane_h264_plugin"
 
   def project do
