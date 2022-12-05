0066a8241ad603aa1ed1d9e0e80f2445ceddd068
varsill <lukasz.kita0@gmail.com>
2022-12-05 14:12:15 +0100

    Revoke modes_test without a reference to ffmpeg

--- test/parser/au_splitter_test.exs
+++ test/parser/au_splitter_test.exs
@@ -1,58 +1,54 @@
-# defmodule AUSplitterTest do
-#   @moduledoc false
+defmodule AUSplitterTest do
+  @moduledoc false
 
-#   use ExUnit.Case
+  use ExUnit.Case
 
-#   alias Membrane.H264.FFmpeg.Parser.Native, as: NativeParser
+  @test_files_names ["10-720a", "10-720p"]
+  @au_lengths_ffmpeg %{
+    "10-720a" => [777, 146, 93],
+    "10-720p" => [25699, 19043, 14379, 14281, 14761, 18702, 14735, 13602, 12094]
+  }
+  defmodule FullBinaryParser do
+    @moduledoc false
+    alias Membrane.H264.Parser.{
+      AUSplitter,
+      NALuParser,
+      NALuSplitter
+    }
 
-#   @test_files_names ["10-720a", "10-720p"]
-#   defmodule FullBinaryParser do
-#     @moduledoc false
-#     alias Membrane.H264.Parser.{
-#       AUSplitter,
-#       NALuParser,
-#       NALuSplitter
-#     }
+    @spec parse(binary()) :: AUSplitter.access_unit_t()
+    def parse(payload) do
+      nalu_splitter = NALuSplitter.new()
+      {nalus_payloads, nalu_splitter} = NALuSplitter.split(payload, nalu_splitter)
+      {last_nalu_payload, _nalu_splitter} = NALuSplitter.flush(nalu_splitter)
+      nalus_payloads = nalus_payloads ++ [last_nalu_payload]
 
-#     @spec parse(binary()) :: AUSplitter.access_unit_t()
-#     def parse(payload) do
-#       nalu_splitter = NALuSplitter.new()
-#       {nalus_payloads, nalu_splitter} = NALuSplitter.split(payload, nalu_splitter)
-#       {last_nalu_payload, _nalu_splitter} = NALuSplitter.flush(nalu_splitter)
-#       nalus_payloads = nalus_payloads ++ [last_nalu_payload]
+      nalu_parser = NALuParser.new()
 
-#       nalu_parser = NALuParser.new()
+      {nalus, _nalu_parser} =
+        Enum.map_reduce(nalus_payloads, nalu_parser, &NALuParser.parse(&1, &2))
 
-#       {nalus, _nalu_parser} =
-#         Enum.map_reduce(nalus_payloads, nalu_parser, &NALuParser.parse(&1, &2))
+      {aus, _au_splitter} = AUSplitter.split(nalus, AUSplitter.new())
 
-#       {aus, _au_splitter} = AUSplitter.split(nalus, AUSplitter.new())
+      aus
+    end
+  end
 
-#       aus
-#     end
-#   end
+  test "if the access unit lenghts parsed by access unit splitter are the same as access units lengths parsed by FFMPEG" do
+    for name <- @test_files_names do
+      full_name = "test/fixtures/input-#{name}.h264"
+      binary = File.read!(full_name)
 
-#   test "if the access unit lenghts parsed by access unit splitter are the same as access units lengths parsed by FFMPEG" do
-#     file_names = Enum.map(@test_files_names, fn name -> "test/fixtures/input-#{name}.h264" end)
+      aus = FullBinaryParser.parse(binary)
 
-#     for file_name <- file_names do
-#       binary = File.read!(file_name)
+      au_lengths =
+        for au <- aus,
+            do:
+              Enum.reduce(au, 0, fn %{payload: payload}, acc ->
+                byte_size(payload) + acc
+              end)
 
-#       aus = FullBinaryParser.parse(binary)
-
-#       au_lengths =
-#         for au <- aus,
-#             do:
-#               Enum.reduce(au, 0, fn %{payload: payload}, acc ->
-#                 byte_size(payload) + acc
-#               end)
-
-#       {:ok, decoder_ref} = NativeParser.create()
-
-#       {:ok, au_lengths_ffmpeg, _decoding_order_numbers, _presentation_order_numbers, _changes} =
-#         NativeParser.parse(binary, decoder_ref)
-
-#       assert au_lengths == au_lengths_ffmpeg
-#     end
-#   end
-# end
+      assert au_lengths == @au_lengths_ffmpeg[name]
+    end
+  end
+end
