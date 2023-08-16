5e97ce2ccb311cb988492d898357bd8f2a46b4fd
Mateusz Front <mateusz.front@swmansion.com>
2023-08-16 15:40:05 +0200

    bump version to 0.6

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.H264.Plugin.Mixfile do
   use Mix.Project
 
-  @version "0.5.1"
+  @version "0.6.0"
   @github_url "https://github.com/membraneframework-labs/membrane_h264_plugin"
 
   def project do
