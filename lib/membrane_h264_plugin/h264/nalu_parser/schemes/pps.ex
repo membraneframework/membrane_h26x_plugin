3279d7804b05d884be8f44859ae4a85019bb4412
Łukasz Kita <lukasz.kita0@gmail.com>
2024-03-13 13:29:56 +0100

    Fix a bug with all the access units being considered a top_field. (#57)

--- lib/membrane_h264_plugin/h264/nalu_parser/schemes/pps.ex
+++ lib/membrane_h264_plugin/h264/nalu_parser/schemes/pps.ex
@@ -10,6 +10,8 @@ defmodule Membrane.H264.NALuParser.Schemes.PPS do
     do: [
       field: {:pic_parameter_set_id, :ue},
       field: {:seq_parameter_set_id, :ue},
+      field: {:entropy_coding_mode_flag, :u1},
+      field: {:bottom_field_pic_order_in_frame_present_flag, :u1},
       save_state_as_global_state: {&{:pps, &1}, [:pic_parameter_set_id]}
     ]
 end
